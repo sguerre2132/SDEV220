b4db719499e46db7b87ae4133b9458deec02d21e
Stephanie <sguerrero6@ivytech.edu>
2025-04-11 19:50:20 -0400

    Add files via upload

---
+++ application.py
@@ -0,0 +1,79 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+db = SQLAlchemy(app)
+
+# Book Model
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    publisher = db.Column(db.String(100), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'book_name': self.book_name,
+            'author': self.author,
+            'publisher': self.publisher
+        }
+def create_tables():
+    db.create_all()
+
+@app.route('/')
+def home():
+    return get_books()
+
+# Get all books
+@app.route('/books', methods=['GET'])
+def get_books():
+    books = Book.query.all()
+    return jsonify([book.to_dict() for book in books])
+
+# Get a single book by ID
+@app.route('/books/<int:id>', methods=['GET'])
+def get_book(id):
+    book = Book.query.get_or_404(id)
+    return jsonify(book.to_dict())
+
+# Add a new book
+@app.route('/books', methods=['POST'])
+def add_book():
+    data = request.get_json()
+    new_book = Book(
+        book_name=data['book_name'],
+        author=data['author'],
+        publisher=data['publisher']
+    )
+    db.session.add(new_book)
+    db.session.commit()
+    return jsonify(new_book.to_dict()), 201
+
+# Update an existing book
+@app.route('/books/<int:id>', methods=['PUT'])
+def update_book(id):
+    book = Book.query.get_or_404(id)
+    data = request.get_json()
+    book.book_name = data['book_name']
+    book.author = data['author']
+    book.publisher = data['publisher']
+    db.session.commit()
+    return jsonify(book.to_dict())
+
+# Delete a book
+@app.route('/books/<int:id>', methods=['DELETE'])
+def delete_book(id):
+    book = Book.query.get_or_404(id)
+    db.session.delete(book)
+    db.session.commit()
+    return jsonify({'message': 'Book deleted'})
+
+# Run the app
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
